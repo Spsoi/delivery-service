57efaf689e56d4a776247605898db7e28d6ea0a8
Spsoi <you@example.com>
2023-04-22 21:44:32 +0300

    add .env

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -11,3 +11,32 @@ function response($data, $status = 200, $headers = [])
     header('Content-Type: application/json');
     echo json_encode($data);
 }
+
+if (!function_exists('env')) {
+    /**
+     * Get the value of the environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $path = dirname(__DIR__, 2) . '/.env';
+
+        if (file_exists($path)) {
+            $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+            foreach ($lines as $line) {
+                if (strpos(trim($line), '#') === 0) {
+                    continue;
+                }
+                [$name, $value] = explode('=', $line, 2);
+                if (trim($name) === $key) {
+                    return trim($value);
+                }
+            }
+        }
+
+        return $default;
+    }
+}
