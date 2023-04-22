a536aa756f4d360d6b5d8ad03f552e07d8ed997c
Spsoi <you@example.com>
2023-04-22 16:02:42 +0300

    Added request routing logic to index.php

--- public/index.php
+++ public/index.php
@@ -2,3 +2,17 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../routes/web.php';
+
+$request_uri = $_SERVER['REQUEST_URI'];
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+foreach ($routes as $route) {
+    if ($request_uri === $route['uri'] && $request_method === $route['method']) {
+        $handler = $route['handler'];
+
+        $controller = new $handler[0];
+        $action = $handler[1];
+        $controller->$action();
+        break;
+    }
+}
