4b7ba4e7caa86b936d1d82d4edcad5acb3fb1931
Spsoi <you@example.com>
2023-04-22 21:26:53 +0300

    add Request.php, and update index.php for Request

---
+++ app/Http/Request/Request.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Request;
+
+class Request
+{
+    public $query;
+
+    public function __construct()
+    {
+    }
+
+    public function getQuery($key, $default = null)
+    {
+        return $this->query[$key] ?? $default;
+    }
+
+    public function setQuery($query)
+    {
+        return $this->query = $query;
+    }
+
+    public function get($key, $default = null)
+    {
+        return $_GET[$key] ?? $default;
+    }
+
+    public function post($key, $default = null)
+    {
+        return $_POST[$key] ?? $default;
+    }
+
+    public function input($key, $default = null)
+    {
+        return $_REQUEST[$key] ?? $default;
+    }
+
+    public function all()
+    {
+        return array_merge($_GET, $_POST);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -6,11 +6,26 @@ require __DIR__ . '/../routes/web.php';
 $request_uri = $_SERVER['REQUEST_URI'];
 $request_method = $_SERVER['REQUEST_METHOD'];
 
+// удалить / перед ?
+$request_uri = str_replace('/?', '?', $request_uri);
+
+// разделим строку $request_uri по символу "?"
+$request_uri_parts = explode('?', $request_uri);
+
+// возьмем первый элемент массива - это и будет наш путь
+$request_uri = $request_uri_parts[0];
+$request = new \App\Http\Request\Request();
+
+if (count($request_uri_parts) > 1) {
+    parse_str($request_uri_parts[1], $query);
+    $request->setQuery($query);
+}
+
 foreach ($routes as $route) {
     if ($request_uri === $route['uri'] && $request_method === $route['method']) {
         $handler = $route['handler'];
 
-        $controller = new $handler[0];
+        $controller = new $handler[0]($request);
         $action = $handler[1];
         $controller->$action();
         break;
