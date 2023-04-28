eff4a53bf44e80849e73cba7c4eb5cbe8b0fb800
Spsoi <you@example.com>
2023-04-28 23:08:03 +0300

    add helpers dd

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -40,3 +40,14 @@ if (!function_exists('env')) {
         return $default;
     }
 }
+
+function dd(...$vars) {
+    echo "<div style='background-color: #f6f8fa; border: 1px solid #ccc; border-radius: 5px; font-family: sans-serif; font-size: 10px; line-height: 1.5; margin: 20px; padding: 20px;'>";
+    foreach ($vars as $var) {
+        echo "<pre>";
+        print_r($var);
+        echo "</pre>";
+    }
+    echo "</div>";
+    die;
+}
