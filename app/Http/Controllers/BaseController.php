b6eba0d401e5410f432180863302142e5684c976
Spsoi <you@example.com>
2023-04-30 19:25:38 +0300

    BaseController add methods

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -14,4 +14,22 @@ class BaseController
     {
         $this->request = $request;
     }
+
+    public function getJsonStream()
+    {
+        $json = file_get_contents('php://input');
+        return json_decode($json);
+    }
+
+    public function getArrayStream()
+    {
+        $json = file_get_contents('php://input');
+        return json_decode($json, true);
+    }
+
+    public function getObjectStream()
+    {
+        $json = file_get_contents('php://input');
+        return json_decode($json);
+    }
 }
